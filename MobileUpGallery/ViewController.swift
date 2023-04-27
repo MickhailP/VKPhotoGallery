3313970428fa4adc2a41ef9c065fbd60d8d8a9f9
Mikhail <mich_p@inbox.ru>
2023-04-27 10:30:29 +0300

    Initial Commit

---
+++ MobileUpGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Миша Перевозчиков on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
