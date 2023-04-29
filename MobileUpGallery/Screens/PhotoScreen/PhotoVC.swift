fa954a04d1a9e0ccf5cb8463d957e53ce1df0a8e
Mikhail <mich_p@inbox.ru>
2023-04-29 22:33:05 +0300

    Добавлен Pinch to Zoom для фото

--- MobileUpGallery/Screens/PhotoScreen/PhotoVC.swift
+++ MobileUpGallery/Screens/PhotoScreen/PhotoVC.swift
@@ -13,8 +13,10 @@ final class PhotoVC: UIViewController {
 
 	let imageView = MUImageView(frame: .zero)
 	
-	let photo: Photo
+	private let photo: Photo
+	private var initialImageWidth: CGFloat?
 
+	let pinch = UIPinchGestureRecognizer()
 
 	init(photo: Photo) {
 		self.photo = photo
@@ -26,11 +28,15 @@ final class PhotoVC: UIViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		configureNavigation(date: photo.date.convertToDateFromUnix())
 		configureImageView(with: photo.photoUrl)
-    }
+
+		addPinchToZoom()
+	}
+
 
 	private func configureNavigation(date: Date) {
 		self.title = date.convertToDayMonthYearFormat()
@@ -41,9 +47,14 @@ final class PhotoVC: UIViewController {
 		navigationController?.navigationBar.prefersLargeTitles = false
 		navigationItem.titleView?.sizeToFit()
 
-
 		let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sharePhoto))
 		self.navigationItem.rightBarButtonItem = shareButton
+	}
+
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		initialImageWidth = imageView.frame.width
 	}
 
 
@@ -51,6 +62,7 @@ final class PhotoVC: UIViewController {
 		view.addSubview(imageView)
 
 		imageView.getImage(from: imageURL)
+		imageView.isUserInteractionEnabled = true
 
 		imageView.snp.makeConstraints { make in
 			make.width.equalToSuperview()
@@ -59,10 +71,16 @@ final class PhotoVC: UIViewController {
 			make.centerY.equalToSuperview()
 		}
 	}
+}
+
+
+//MARK: Activity screen
+extension PhotoVC {
 
 	@objc func sharePhoto() {
 		configureActivityVC()
 	}
+
 
 	private func configureActivityVC() {
 		guard let image = imageView.image, image != Images.placeholder else {
@@ -72,5 +90,23 @@ final class PhotoVC: UIViewController {
 
 		let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
 		self.present(activityVC, animated: true)
+	}
+}
+
+
+//MARK: Pinch to Zoom
+extension PhotoVC {
+
+	private func addPinchToZoom() {
+		imageView.addGestureRecognizer(pinch)
+		pinch.addTarget(self, action: #selector(didPinch))
+	}
+
+
+	@objc func didPinch(_ gesture: UIPinchGestureRecognizer) {
+		guard let gestureView = pinch.view else { return }
+
+		gestureView.transform = gestureView.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+		pinch.scale = 1
 	}
 }
